6b1ed282cc9aa809fbd5c7cb5abacc84b53cf0ab
Srushti Pawar <pawarsrushti15@gmail.com>
2026-08-04 13:29:29 +0530

    Solve 0001-Two-Sum.java

--- Arrays/0001-Two-Sum.java
+++ Arrays/0001-Two-Sum.java
@@ -1,1 +1,34 @@
+/*
+LeetCode: 1
+Problem: Two Sum
+Difficulty: Easy
 
+Approach:
+- Use a HashMap to store previously seen numbers.
+- Check whether the complement exists.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int diff = target - nums[i];
+
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
